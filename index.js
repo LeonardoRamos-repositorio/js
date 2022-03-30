5658791fc40fe6ec6309ad298bbddd60e15530e0
Leonardo Ramos <llramoss@gmail.com>
2022-03-30 13:06:28 -0600

    primer commit para entregable

---
+++ index.js
@@ -0,0 +1,13 @@
+let numero1 = parseInt(prompt("Introduce número a evaluar "));
+let numero2 = parseInt(prompt("Segundo número a evaluar"));
+
+if (numero1 <= numero2){
+    alert("el primer número " + numero1 + " es menor al segundo " + numero2 )
+}else if (numero1 >= numero2){
+    alert("el primer número " + numero1 + " es mayor al segundo " + numero2 )
+}else {
+    alert("el primer número " + numero1 + " es igual al segundo " + numero2 )
+}
+
+
+
