a622827356ae5e23da7f654786a95a97b37c2149
Leonardo Ramos <llramoss@gmail.com>
2022-03-30 13:36:53 -0600

    segundo commit para entregable

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 let numero1 = parseInt(prompt("Introduce número a evaluar "));
 let numero2 = parseInt(prompt("Segundo número a evaluar"));
 
-if (numero1 <= numero2){
-    alert("el primer número " + numero1 + " es menor al segundo " + numero2 )
-}else if (numero1 >= numero2){
-    alert("el primer número " + numero1 + " es mayor al segundo " + numero2 )
-}else {
-    alert("el primer número " + numero1 + " es igual al segundo " + numero2 )
+if (numero1 < numero2){
+    alert("el primer número " + numero1 + " es menor al segundo " + numero2 );
+}else if (numero1 > numero2){
+    alert("el primer número " + numero1 + " es mayor al segundo " + numero2 );
+}else if (numero1 == numero2){
+    alert("el primer número " + numero1 + " es igual al segundo " + numero2 );
 }
 
+
+alert("Con la suma de los numeros imprimira y se repetira en consola");
+let suma = numero1 + numero2;
+console.log("se imprime en consola la suma desde el  1 al " + suma + " con ciclo FOR");
+for (let i = 1; i <= suma; i++){
+    console.log(i)
+}
 
 
