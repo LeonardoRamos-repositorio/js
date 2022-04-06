86787e2b7c0b465ef3cf0b9858939c7b3f272e55
Leonardo Ramos <llramoss@gmail.com>
2022-04-05 23:30:12 -0500

    entrega dos mod

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-// let numero1 = parseInt(prompt("Introduce número a evaluar "));
-// let numero2 = parseInt(prompt("Segundo número a evaluar"));
-// if (!isNaN(numero1) && !isNaN(numero2)){
-    
-//     if (numero1 < numero2){
-//         alert("el primer número " + numero1 + " es menor al segundo " + numero2 );
-//     }else if (numero1 > numero2){
-//         alert("el primer número " + numero1 + " es mayor al segundo " + numero2 );
-//     }else if (numero1 == numero2){
-//         alert("el primer número " + numero1 + " es igual al segundo " + numero2 );
-//     }
-
-
-//     alert("Con la suma de los numeros imprimira y se repetira en consola");
-//     let suma = numero1 + numero2;
-//     console.log("se imprime en consola la suma desde el  1 al " + suma + " con ciclo FOR");
-//     for (let i = 1; i <= suma; i++){
-//     console.log(i)
-//     }
-
-    
-// }else
-//     alert("No has introducido números")
-
-
 alert("¿Qué te gustaría encontrar en tu proceso psicoterapéutico? – selección múltiple. Debes escoger dos (2) opciones.");
 alert("selecciona una de las opciones de la pantalla")
 let opciones = 7 
