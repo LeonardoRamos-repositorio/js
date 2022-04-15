bb1a1c19211a550ddbed6e121146031757c28a57
Leonardo Ramos <llramoss@gmail.com>
2022-04-14 21:43:38 -0500

    avanzando

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
-// alert("¿Qué te gustaría encontrar en tu proceso psicoterapéutico? – selección múltiple. Debes escoger dos (2) opciones.");
-// alert("selecciona una de las opciones de la pantalla")
-// let opciones = 7 
-// let opcion1 = prompt("selecciona una de las opciones (1 al 7)");
-// let opcion2 = prompt("selecciona una de las opciones (1 al 7)");
-//  if (!isNaN(opcion1) && !isNaN(opcion2)){
-//     if (opcion1 >opciones){
-//         alert("intenta de nuevo");
-//     }else elije_terapeuta()
-        
+class Consultorio{
+  contructor(nombreConsultorio, anioApertura, fundador){
+    this.nombreConsultorio = nombreConsultorio;
+    this.anioApertura = anioApertura;
+    this.fundador = fundador;
 
-// }else  alert("No has introducido números");
+  }
+}
 
-// function elije_terapeuta() {
-//   alert("Elije un servicio terapeutico");
+const amarTMas = new Consultorio("Amarte+", 2007, "Grupo Psicologico");
 
-//   const precioterapeuta= [9.99, 7.99, 4.99];
-//   const eleccion = parseInt(
-//     prompt(
-//       "Ingresa tu eleccion" + " 1- Experto 2- Especialista 3- Profecional"
-//     )
-//   );
-
-//   if (eleccion < 1 || eleccion > 3) {
-//     alert("Opcion no valida");
-//     elije_terapeuta();
-
-//     return;
-//   }
-
-//   alert(
-//     "se ha agregado un servicio y se han sumado " +
-//       precioterapeuta[eleccion - 1] +
-//       " USD a tu cuenta"
-//   );
-//   terapeutaSeleccionado.elije_terapeuta = precioterapeuta[eleccion - 1];
-
-  
-// }
 
 class  Terapeuta {
-  constructor (nombre, nacionalidad, modalidad, precio, tipo){
+  constructor (nombre, nacionalidad, modalidad, precio, tipos){
     this.nombre = nombre.toUpperCase()
     this.nacionalidad = nacionalidad
     this.modalidad = modalidad
@@ -47,11 +19,11 @@ class  Terapeuta {
     this.tipos = []
   }
 
-    agregarTipo(tipo){
-      this.tipo.push(tipo)
+    agregarTipo(tipos){
+      this.tipos.push(tipos)
     }
 }
-class Tipo{
+class Tipos{
   constructor(nombreTerapeuta, tipoTerapeuta, atencion){
     this.nombreTerapeuta = nombreTerapeuta;
     this.tipoTerapeuta = tipoTerapeuta;
@@ -71,10 +43,18 @@ class Cuenta {
     this.sesiones.push(sesiones)
   }
 }
+const cuenta1 = new Cuenta("Antonio", "1 sesion", "videoTerapia", "4.99");
+const cuenta2 = new Cuenta("Gerardo", "1 semana", "ChatTerapia", "7.99");
+const cuenta3 = new Cuenta("Pedro", "2 meses", "videoTerapia", "9.99");
+
+
+
+
+
 const paciente = []
 paciente.push(new Cuenta("Antonio", "1 sesion", "videoTerapia", "4.99"))
 
-console.log(Cuenta);
+console.log(paciente);
 
 const experto = []
 experto.push(new Terapeuta("Gabriel ", "Peruana", "Videollamada", 9.99, "Experto"))
@@ -95,3 +75,6 @@ profecional.push(new Terapeuta("Macario", "Argentina", "Chat", 4.99, "Profeciona
 
 
 console.log(profecional);
+
+
+console.log(amarTMas);
